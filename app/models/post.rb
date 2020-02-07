0a9f5041c550279c593622187dce919f78f22ba5
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-02-07 00:05:26 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,6 +10,11 @@ class Post < ActiveRecord::Base
       errors.add(:title, "Title must be in title case")
     end
   end
+  
+  def is_title_case
+    if title.split.any?{|w|w[0].upcase != w[0]}
+      errors.add(:title, "")
+  end
 
   def make_title_case
     self.title = self.title.titlecase
